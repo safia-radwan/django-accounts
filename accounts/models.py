1613ba4a391b1176fb4b66c37ee99c21a393b174
safia <safiaradwan950@gmail.com>
2025-10-12 04:57:53 +0200

    add profile model to the base user and add create profile signal

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
 
 # Create your models here.
+class profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=11, null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    # profile_pic = models.ImageField(upload_to='profile_pics', null=True, blank=True)
+    def __str__(self):
+        return self.user.username
+    
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender,instance,created,**kwargs):
+        if created :
+            profile.objects.create(user=instance)
+
+
+        
